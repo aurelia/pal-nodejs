f796e86acd9dd48a2e88ea1eab9876e09f412041
Meirion Hughes <mhughes@geologging.com>
2016-07-28 11:55:36 +0100

    initial implementation and alpha release

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var shell = require('gulp-shell');
+var merge = require('merge2');
+var jasmine = require('gulp-jasmine');
+var plumber = require('gulp-plumber');
+var sourcemap = require('gulp-sourcemaps');
+var rimraf = require('gulp-rimraf');
+var replace = require('gulp-replace');
+var runsequence = require('run-sequence');
+
+
+
+var paths = {
+    source: "source/",
+    output: "dist/",
+    spec: "spec/"
+}
+
+gulp.task('clean:typescript', function () {
+    return gulp.src([paths.output + '**/*'], { read: false })
+        .pipe(rimraf());
+});
+
+gulp.task('clean:tests', function () {
+    return gulp.src([paths.spec + '**/*.spec.js', paths.spec + '**/*.map'], { read: false })
+        .pipe(rimraf());
+});
+
+gulp.task('clean', ['clean:tests', 'clean:typescript'], function () {
+});
+
+gulp.task('compile:typescript', ['clean:typescript'], function () {
+    var project = ts.createProject('tsconfig.json');
+
+    var tsResult = gulp
+        .src([
+            '!' + paths.source + '**/*spec.ts',
+            paths.source + '**/*.ts',
+            'typings/index.d.ts'
+        ])
+        .pipe(sourcemap.init())
+        .pipe(ts(project));
+
+    return merge([
+        tsResult.dts.pipe(gulp.dest(paths.output)),
+        tsResult.js
+            .pipe(sourcemap.write('.', { sourceRoot: '../source' }))
+            .pipe(gulp.dest(paths.output))
+    ]);
+});
+
+gulp.task('compile:tests', ['compile:typescript', 'clean:tests'], function () {
+    var project = ts.createProject('tsconfig.json');
+
+    var tsResult = gulp.src([
+        paths.spec + '**/*spec.ts', 'typings/index.d.ts'
+    ])
+        .pipe(sourcemap.init())
+        .pipe(ts(project));
+
+    return tsResult.js
+        .pipe(sourcemap.write('.', { sourceRoot: '../spec' }))
+        .pipe(replace(/(require\('\..\/source\/)/g, 'require(\'..\/dist\/'))
+        .pipe(gulp.dest(paths.spec));
+});
+
+gulp.task('test:jasmine', function (done) {
+    return gulp.src('spec/*.js')
+        .pipe(plumber())
+        .pipe(jasmine({ verbose: true }));
+});
+
+gulp.task('test', function (done) {
+    runsequence('compile:tests', 'test:jasmine', function (err) {
+        runsequence('clean:tests');
+        done();
+    });
+});
+
+gulp.task('test-no-compile', function (done) {
+    runsequence('test:jasmine', function (err) {
+        runsequence('clean:tests');
+        done();
+    });
+});
+
+gulp.task('watch', ['test'], function () {
+    gulp.watch(paths.source + '**/*.ts', ['test']);
+});
+
+gulp.task('default', ['test'], function () {
+});
---
+++ source/nodejs-mutation-emulator.ts
@@ -0,0 +1,187 @@
+import {EventEmitter} from 'events';
+import {IObserver} from './observer';
+import {IDisposable} from './disposeable';
+
+export class NodeJsMutationEmulator {
+  private cycleMutations: MutationRecord[];
+  private cycleTimerId: NodeJS.Timer;
+  private targets: { target: Node, last: Node }[];
+  private observers: IObserver[];
+  public interval:number;
+
+  constructor(interval?: number) {
+    this.interval = (interval === undefined) ? 100 : interval;
+
+    this.targets = [];
+    this.cycleMutations = [];
+    this.observers = [];
+  }
+
+  start(){
+     if (this.interval > 0)
+      this.cycleTimerId = setInterval(() => this.cycle(), this.interval);
+  }
+  
+  stop(){    
+    clearInterval(this.cycleTimerId);
+  }
+
+  registerMutation(mutation: MutationRecord) {
+    this.cycleMutations.push(mutation);
+  }
+
+  registerObserver(observer: IObserver): IDisposable {
+    this.observers.push(observer);
+
+    var target = observer.target;
+    var entry = this.targets.find(x => x.target == target);
+
+    if (!entry) {
+      entry = {
+        target: target,
+        last: target.cloneNode(true),
+      }
+      this.targets.push(entry);
+    }
+
+    return () => {
+      var index = this.observers.indexOf(observer);
+      if (index != -1)
+        this.observers.splice(index, 1);
+    }
+  }
+
+  public cycle() {
+    this.cycleDirtyCheck();
+    this.cycleReport();
+    this.cycleTidy();
+  }
+
+  private cycleTidy() {
+    //remove targets no longer having any observer;  
+  }
+
+  private cycleReport() {
+    let mutations = this.cycleMutations;
+    this.cycleMutations = [];
+
+    let count = mutations.length;
+
+    this.observers.forEach(observer => {
+      let observerMutations: MutationRecord[] = [];
+
+      for (let i = 0; i < count; i++) {
+        let mutation = mutations[i];
+        if (observer.target == mutation.target)
+          observerMutations.push(mutation);
+      }
+      if (observerMutations.length > 0)
+        observer.callback(observerMutations);
+    });
+  }
+
+  private cycleDirtyCheck() {
+    let targets = this.targets;
+    let targetsCount = targets.length;
+
+    for (let i = 0; i < targetsCount; i++) {
+      let target = targets[i];
+      let targetNode = target.target;
+      let previous = target.last;
+
+      this.cycleDirtyCheckAttributes(targetNode, previous);
+      this.cycleDirtyCheckNodeValue(targetNode, previous);
+      this.cycleDirtyCheckChildList(targetNode, previous);     
+
+      target.last = targetNode.cloneNode(true);
+    }
+  }
+  
+  private cycleDirtyCheckAttributes(target: Node, previous:Node ) {
+    if(!target.attributes)
+        return;
+
+    let attrNew = target.attributes;
+    let attrOld = previous.attributes;
+    let countNew = attrNew.length;
+
+    for (let i = 0; i < countNew; i++) {
+      let attr = attrNew.item(i);
+      let old = this.findAttr(attrOld, attr.name);
+
+      let mutated = false;
+
+      if (!old || old.value != attr.value) {
+        let mutation = <MutationRecord>{
+          target: target,
+          type: "attributes",
+          addedNodes: {},
+          removedNodes: {},
+          previousSibling: null,
+          nextSibling: null,
+          oldValue: old.value,
+          attributeName: attr.name,
+          attributeNamespace: attr.namespaceURI,
+        }
+        this.registerMutation(mutation);
+      }
+    }
+
+    let countOld = attrOld.length;
+
+    for (let i = 0; i < countOld; i++) {
+
+      let old = attrOld.item(i);
+
+      if (!this.findAttr(attrNew, old.name)) {
+        let mutation = <MutationRecord>{
+          target: target,
+          type: "attributes",
+          addedNodes: {},
+          removedNodes: {},
+          previousSibling: null,
+          nextSibling: null,
+          oldValue: old.value,
+          attributeName: old.name,
+          attributeNamespace: old.namespaceURI,
+        }
+
+        this.registerMutation(mutation);
+      }
+    }
+  }
+
+  private cycleDirtyCheckNodeValue(target: Node, previous:Node ) {
+    if (target.nodeValue != previous.nodeValue) {
+        let mutation = <MutationRecord>{
+          target: target,
+          type: "characterData",
+          addedNodes: {},
+          removedNodes: {},
+          previousSibling: null,
+          nextSibling: null,
+          oldValue: previous.nodeValue,
+          attributeName: null,
+          attributeNamespace: null,
+        }
+
+        this.registerMutation(mutation);
+    }
+  }
+  
+  private cycleDirtyCheckChildList(target: Node, previous:Node ) {
+    if(!target.nodeValue)
+        return;
+  }  
+
+  private findAttr(attrs: NamedNodeMap, name: string): Attr {
+    var count = attrs.length;
+
+    for (let i = 0; i < count; i++) {
+      var attr = attrs.item(i);
+      if (attr.name == name)
+        return attr;
+    }
+    return null;
+  }
+}
