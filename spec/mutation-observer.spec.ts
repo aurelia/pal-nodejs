5e64d09d3633b03bcce4609a9cb24a0edb00afe7
Meirion Hughes <mhughes@geologging.com>
2016-10-01 22:50:56 +0100

    test(mutation): child-list mutation events

--- spec/mutation-observer.spec.ts
+++ spec/mutation-observer.spec.ts
@@ -58,4 +58,35 @@ describe("MutationObserver", () => {
 
     text.nodeValue = "B";
   });
+
+  it("child-node changes trigger mutation event", function (done) {
+    var pal = buildPal();
+    var dom = pal.dom;
+    var document = pal.global.window.document;
+    var parent = document.createElement("div");
+    var child = document.createElement("p");
+
+    parent.appendChild(child);
+
+    var observer = dom.createMutationObserver((changes) => {
+      try {
+        expect(changes.length).toBe(1);
+        expect(changes[0].removedNodes.length).toBe(1);
+        expect(changes[0].removedNodes[0]).toBe(child);
+      }
+      catch (err) {
+        fail();
+      }
+      finally {
+        observer.disconnect();
+        done();
+      }
+    });
+
+    observer.observe(parent, {
+      childList: true,
+    });
+
+    parent.removeChild(child);      
+  });
 });
