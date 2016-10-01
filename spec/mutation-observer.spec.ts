d93f5587f0382a4d5052de17b33d51678ad2ad49
Devan Patel <devanp92@users.noreply.github.com>
2016-10-01 19:35:37 -0400

    test(MO + pal-builder) (#10)

--- spec/mutation-observer.spec.ts
+++ spec/mutation-observer.spec.ts
@@ -1,6 +1,24 @@
 import { buildPal } from '../src/nodejs-pal-builder';
 
-describe("MutationObserver", () => {
+describe("mutation-observer", () => {
+  let pal, observer, dom, document;
+  const noop = () => {};
+
+  beforeEach(() => {
+    pal = buildPal();
+    dom = pal.dom;
+    document = pal.global.window.document;
+    observer = dom.createMutationObserver(noop);
+  });
+
+  afterEach(() => {
+    observer.disconnect();
+  });
+
+  it('should be defined', () => {
+    expect(observer).toBeDefined();
+  });
+
   it("attribute changes trigger mutation event", function (done) {
     var pal = buildPal();
     var dom = pal.dom;
@@ -26,7 +44,7 @@ describe("MutationObserver", () => {
 
     observer.observe(div, {
       attributes: true
-      
+
     });
 
     div.setAttribute('a', 'B');
@@ -87,10 +105,10 @@ describe("MutationObserver", () => {
       childList: true,
     });
 
-    parent.removeChild(child);      
+    parent.removeChild(child);
   });
 
-   it("subtree child-node changes trigger mutation event", function (done) {
+  it("subtree child-node changes trigger mutation event", function (done) {
     var pal = buildPal();
     var dom = pal.dom;
     var document = pal.global.window.document;
@@ -122,6 +140,127 @@ describe("MutationObserver", () => {
       subtree: true,
     });
 
-    child.removeChild(childchild);      
+    child.removeChild(childchild);
+  });
+
+  it('should not detect no changes', () => {
+    const target = document.createElement('div');
+
+    target.setAttribute('attribute', 'attribute');
+
+    observer.observe(target, {
+      attributes: true,
+      attributeOldValue: true
+    });
+
+    let records = observer.takeRecords();
+
+    expect(records.length).toBe(0);
+
+    target.setAttribute('attribute', 'attribute');
+
+    records = observer.takeRecords();
+
+    expect(records.length).toBe(0);
+  });
+
+  it('should detect changes for a single element', () => {
+    const target = document.createElement('div');
+
+    target.setAttribute('class', 100);
+
+    observer.observe(target, {
+      attributes: true,
+      attributeOldValue: true
+    });
+
+    target.removeAttribute('class');
+
+    const records = observer.takeRecords();
+
+    expect(records.length).toBe(1);
+    expect(records[0].oldValue).toBe('100');
+  });
+
+  it('should detect changes for a parents elements', () => {
+    const target = document.createElement('div');
+    const child = document.createElement('div');
+
+    target.setAttribute('id', 'parent');
+    child.setAttribute('id', 'child');
+    target.appendChild(child);
+
+    observer.observe(target, {
+      attributes: true,
+      childList: true
+    });
+
+    target.removeAttribute('id');
+
+    const records = observer.takeRecords();
+
+    expect(records.length).toBe(1);
+    expect(records[0].oldValue).toBe('parent');
+  });
+
+  it('should detect removing child nodes', () => {
+    const target = document.createElement('div');
+    const child = document.createTextNode(`I'm a text node`);
+
+    target.setAttribute('id', 'parent');
+    target.appendChild(child);
+
+    observer.observe(target, {
+      attributes: true,
+      childList: true
+    });
+
+    target.removeChild(child);
+
+    const records = observer.takeRecords();
+
+    expect(records.length).toBe(1);
+    expect(records[0].removedNodes).toBeDefined();
+  });
+
+  it('should detect both parent and child changes', () => {
+    const target = document.createElement('div');
+    const child = document.createElement('div');
+
+    target.setAttribute('id', 'parent');
+    child.setAttribute('id', 'child');
+
+    target.appendChild(child);
+
+    observer.observe(target, {
+      attributes: true,
+      childList: true,
+      attributeOldValue: true
+    });
+
+    target.setAttribute('id', 'changedParent');
+    child.setAttribute('id', 'changedChild');
+
+    const records = observer.takeRecords();
+
+    expect(records.length).toBe(2);
+    expect(records[0].oldValue).toBe('parent');
+    expect(records[1].oldValue).toBe('child');
+  });
+
+  xit('should detect changes for a single text node', () => {
+    const textNode = document.createTextNode(`I'm a text node`);
+
+    observer.observe(textNode, {
+      attributes: true,
+      attributeOldValue: true
+    });
+
+    textNode.innerText = 'class';
+
+    const records = observer.takeRecords();
+
+    expect(records.length).toBe(1);
+    expect(records[0].oldValue).toBe('100');
   });
 });
