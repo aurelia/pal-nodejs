a414eb7e901c3d7e7183f53320247945a3bf2227
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-11-20 00:11:29 +0100

    feat(whole-text): add @jdanyow's wholeText jsdom polyfill

---
+++ src/polyfills/jsdom-whole-text.ts
@@ -0,0 +1,25 @@
+import {implementation as TextImpl} from 'jsdom/lib/jsdom/living/nodes/Text-impl';
+import * as NODE_TYPE from 'jsdom/lib/jsdom/living/node-type';
+import {domSymbolTree} from 'jsdom/lib/jsdom/living/helpers/internal-constants';
+
+export function polyfillWholeText() {
+  Object.defineProperty(TextImpl.prototype, 'wholeText', {
+    get: function() {
+      let wholeText = this.textContent;
+      let next;
+      let current = this;
+      while ((next = domSymbolTree.previousSibling(current)) && next.nodeType === NODE_TYPE.TEXT_NODE) {
+        wholeText = next.textContent + wholeText;
+        current = next;
+      }
+      current = this;
+      while ((next = domSymbolTree.nextSibling(current)) && next.nodeType === NODE_TYPE.TEXT_NODE) {
+        wholeText += next.textContent;
+        current = next;
+      }
+      return wholeText;
+    },
+    enumerable: true,
+    configurable: true
+  })
+}
---
+++ src/polyfills/jsdom.d.ts
@@ -0,0 +1,3 @@
+declare module 'jsdom/lib/jsdom/living/nodes/Text-impl';
+declare module 'jsdom/lib/jsdom/living/node-type';
+declare module 'jsdom/lib/jsdom/living/helpers/internal-constants';
