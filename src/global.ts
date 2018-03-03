fa2bbd3b19f688c60a3d6e0d06d7744f93e1754d
EisenbergEffect <rob@bluespire.com>
2018-03-03 15:37:38 -0800

    fix(global): add missing interface element to fix compile errors

--- src/global.ts
+++ src/global.ts
@@ -3,6 +3,7 @@ import { DOMWindow, JSDOM } from 'jsdom';
 export interface IGlobal extends DOMWindow {
   MutationObserver: typeof MutationObserver;
   Element: typeof Element;
+  NodeList: typeof NodeList;
   jsdom: typeof JSDOM;
   SVGElement: typeof SVGElement;
   XMLHttpRequest: typeof XMLHttpRequest;
