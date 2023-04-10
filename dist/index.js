f4ce628603dbe8b6426e740c292908b0a1f0c348
Matthieu COLLÉ <druidev+github@gmail.com>
2023-04-10 17:18:33 -0400

    fix(Index): filter out undefined

--- dist/index.js
+++ dist/index.js
@@ -66,8 +66,12 @@ function initialize() {
 exports.initialize = initialize;
 function createBrowserGlobals() {
     Object.getOwnPropertyNames(aurelia_pal_1.PLATFORM.global)
-        .filter(prop => prop !== 'undefined' && typeof global[prop] === 'undefined')
-        .forEach(prop => global[prop] = aurelia_pal_1.PLATFORM.global[prop]);
+        .filter(prop => typeof global[prop] === 'undefined')
+        .forEach(prop => {
+          if (prop !== 'undefined') {
+            global[prop] = aurelia_pal_1.PLATFORM.global[prop]
+          }
+        });
 }
 function globalize() {
     initialize();
